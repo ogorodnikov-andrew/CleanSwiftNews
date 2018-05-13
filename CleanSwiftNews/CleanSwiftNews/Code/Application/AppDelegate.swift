f92ad7f4c687bb5f46d27191b691d70465cacec8
Andrei Ogorodnikov <ogorodnikov.andrew@gmail.com>
2018-05-13 20:00:07 +0300

    Create Xcode project. Remove automatically generated view controller. Setup project settings. Refactor default project structure.

---
+++ CleanSwiftNews/CleanSwiftNews/Code/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CleanSwiftNews
+//
+//  Created by Andrei Ogorodnikov on 13/05/2018.
+//  Copyright © 2018 Andrei Ogorodnikov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+  ) -> Bool {
+    return true
+  }
+}
